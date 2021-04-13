cbda7742577f01db0afecb7cf91e4fbfe6a7e183
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-04-13 23:33:59 +0530

    Added: Code for Valid FirstName

---
+++ userMain.java
@@ -0,0 +1,28 @@
+package userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class userMain {
+	public static boolean isFirstName(String firstname) {
+		String  regex = "^[A-Z]{1}[a-z]{2,}";
+		Pattern patt = Pattern.compile(regex);
+		if(firstname == null) {
+			return false;
+		}
+		Matcher match = patt.matcher(firstname);
+		return match.matches();
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter firstname:");
+		String firstname = input.nextLine();
+		if(isFirstName(firstname) == true) {
+			System.out.println("Firstname is Correct");
+		}else {
+			System.out.println("Firstname is Incorrect");
+		}
+	}
+}
