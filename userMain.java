76fe57b5bc1ee835502be2c39cd204cf4d74ce5e
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-04-14 15:12:24 +0530

    Added: Code for exactly one Special Character in the password.

--- userMain.java
+++ userMain.java
@@ -47,7 +47,7 @@ public class userMain {
 	
 	//Rule4 - Should have exactly 1 Special Character
 	public static boolean isPasswordValid(String password) {
-		String regex = "[a-zA-Z0-9]{8,}$";
+		String regex = "^[a-zA-Z0-9]+[@#$%&*_+=!?]?$";
 		Pattern patt = Pattern.compile(regex);
 		if(password == null) {
 			return false;
